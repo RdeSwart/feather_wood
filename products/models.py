865dbefda0f318f9a0640a700131c2c78b2f6126
RdeSwart <ruthdeswart@outlook.ie>
2025-03-04 16:10:46 +0000

    feat: Add products app and Create models

---
+++ products/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    brand = models.ForeignKey('Brand', null=True, blank=True, on_delete=models.SET_NULL)
+    details = models.CharField(max_length=250)
+    description = models.TextField()
+    rrp_price = models.DecimalField(max_digits=6, decimal_places=2)
+    discount = models.DecimalField(max_digits=6, decimal_places=2)
+    discounted_price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    sale_price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    @property
+    def discounted_price(self):
+        return((self.rrp_price)*(self.discount))/100
+    
+    @property
+    def sale_price(self):
+        return (self.rrp_price)-(self.discounted_price)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+    friendly_name = models.CharField(max_length=300, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+    
+
+class Brand(models.Model):
+    name = models.CharField(max_length=300)
+    friendly_name = models.CharField(max_length=300, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
