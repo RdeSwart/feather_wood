811266c77e2621682b9a314e793f909e519afb0d
RdeSwart <ruthdeswart@outlook.ie>
2025-03-09 13:23:42 +0000

    feat: Set up products app including urls and html template page

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
+def all_products(request):
+    """
+    This view returns a list of all products, including
+    a search and sort feature
+    """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
