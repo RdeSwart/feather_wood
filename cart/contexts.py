cdfe88c1d39b980f154605aceefb78e339aa9abc
RdeSwart <ruthdeswart@outlook.ie>
2025-03-16 13:03:10 +0000

    feat: Create add product to cart form including views and urls

---
+++ cart/contexts.py
@@ -0,0 +1,45 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
+
+def cart_contents(request):
+
+    cart_items = []
+    total = 0
+    product_count = 0
+    delivery = 6.99
+    cart = request.session.get('cart', {})
+
+    for item_id, item_data in cart.items():
+
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            total += quantity * product.price
+            product_count += quantity
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': quantity,
+                'product': product,
+            })
+
+    grand_total = delivery + total
+
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'grand_total': grand_total,
+    }
+
+    return context
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def cart(request):
@@ -6,3 +6,20 @@ def cart(request):
     This view returns the Shopping Cart Page
     """
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the product to the shopping cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
