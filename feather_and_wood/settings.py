b99721c6d7623659e1be95a0453b08949765fc87
RdeSwart <ruthdeswart@outlook.ie>
2025-04-28 17:11:29 +0100

    fix: Put in correct storage bucket name for deployment

--- feather_and_wood/settings.py
+++ feather_and_wood/settings.py
@@ -180,7 +180,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'feather-and-wood-pp5'
+    AWS_STORAGE_BUCKET_NAME = 'feather-and-wood'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
