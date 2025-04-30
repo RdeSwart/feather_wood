a917f9c557764312d56f390c45850c9b36ddce6d
RdeSwart <ruthdeswart@outlook.ie>
2025-04-30 11:52:06 +0100

    fix: Move query indentation inside if block to work

--- products/views.py
+++ products/views.py
@@ -57,11 +57,11 @@ def all_products(request):
                 )
                 return redirect(reverse('products'))
 
-        queries = (
-            Q(name__icontains=query) |
-            Q(description__icontains=query)
-        )
-        products = products.filter(queries)
+            queries = (
+                Q(name__icontains=query) |
+                Q(description__icontains=query)
+            )
+            products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
 
