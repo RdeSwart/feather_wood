77eff56691ac44518884f637a52ad932430c5af5
RdeSwart <ruthdeswart@outlook.ie>
2025-05-12 17:24:54 +0100

    fix: Add wishlist view to home index page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product, WishlistItem
 
 
 def index(request):
-
     """
     This view returns the Home Page rendering products on sale.
     """
@@ -12,7 +11,14 @@ def index(request):
         product for product in products_on_sale if product.sale_price < product.rrp_price
     ]
 
-    return render(request, 'home/index.html', {'products': products_on_sale})
+    wishlist_items = []
+    if request.user.is_authenticated:
+        wishlist_items = WishlistItem.objects.filter(user=request.user).values_list('product_id', flat=True)
+
+    return render(request, 'home/index.html', {
+        'products': products_on_sale,
+        'wishlist_items': wishlist_items,
+    })
 
 
 def about(request):
